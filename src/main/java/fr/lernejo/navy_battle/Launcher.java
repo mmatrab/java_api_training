f70a0e0cdd631e37908239e6d4647b9a67c132fa
mohamed matrab <mohamed.matrab@epita.fr>
2023-05-26 12:11:37 +0200

    Some changer on Launcher

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,6 +1,5 @@
 package fr.lernejo.navy_battle;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -8,8 +7,6 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.Executors;
 
 public class Launcher {
@@ -41,43 +38,6 @@ public class Launcher {
             String response = "OK";
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }
-    }
-    static class GameStartHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange exchange) throws IOException {
-            if (!"POST".equalsIgnoreCase(exchange.getRequestMethod())) {
-                sendResponse(exchange, 404, "");
-                return;
-            }
-
-            ObjectMapper mapper = new ObjectMapper();
-            Map requestJson;
-
-            try {
-                requestJson = mapper.readValue(exchange.getRequestBody(), Map.class);
-                if (!requestJson.containsKey("id") || !requestJson.containsKey("url") || !requestJson.containsKey("message")) {
-                    throw new IllegalArgumentException();
-                }
-            } catch (Exception e) {
-                sendResponse(exchange, 400, "");
-                return;
-            }
-
-            Map<String, String> responseJson = Map.of(
-                "id", UUID.randomUUID().toString(),
-                "url", "http://localhost:" + exchange.getLocalAddress().getPort(),
-                "message", "May the best code win"
-            );
-
-            sendResponse(exchange, 202, mapper.writeValueAsString(responseJson));
-        }
-
-        private void sendResponse(HttpExchange exchange, int status, String response) throws IOException {
-            exchange.sendResponseHeaders(status, response.length());
-            OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
             os.close();
         }
